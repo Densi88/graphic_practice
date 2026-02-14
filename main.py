c9c57b3213d934a63dbe6f6a9f1c76235cf136f0
densi <ignatiev1904.ph@mail.ru>
2026-02-14 20:11:12 +0800

    adding buttons

--- main.py
+++ main.py
@@ -7,11 +7,51 @@ display.set_caption("типа пеинт")
 screen=display.set_mode((800, 800))
 
 running =True
-startDraw=False
 first_pos=None
 WHITE=(255,255,255)
 BLACK=(0, 0, 0)
+GRAY = (200, 200, 200) 
+BLUE = (100, 150, 255)
 screen.fill(WHITE)
+button_circle=pygame.Surface((120, 40))
+button_rectangle=pygame.Surface((120, 40))
+button_polygon=pygame.Surface((120, 40))
+button_polygon.fill(GRAY)
+button_circle.fill(GRAY)
+button_rectangle.fill(GRAY)
+font=pygame.font.Font(None, 14)
+
+text = font.render("Circle", True, BLACK)
+text_button = text.get_rect(
+    center=(button_circle.get_width() /2, 
+            button_circle.get_height()/2))
+
+text2 = font.render("Rectangle", True, BLACK)
+text_button2 = text2.get_rect(
+    center=(button_circle.get_width() /2, 
+            button_circle.get_height()/2))
+
+text3 = font.render("Polygone", True, BLACK)
+text_button3 = text3.get_rect(
+    center=(button_polygon.get_width() /2, 
+            button_polygon.get_height()/2))
+
+button_rect = pygame.Rect(25, 10, 150, 50)
+button_rect2 = pygame.Rect(175, 10, 150, 50)
+button_rect3 = pygame.Rect(325, 10, 150, 50)
+
+
+button_circle.blit(text, text_button)
+button_rectangle.blit(text2, text_button2)
+button_polygon.blit(text3, text_button3)
+
+pygame.draw.rect(button_circle, BLACK, button_circle.get_rect(), 2)
+pygame.draw.rect(button_rectangle, BLACK, button_rectangle.get_rect(), 2)
+pygame.draw.rect(button_polygon, BLACK, button_polygon.get_rect(), 2)
+
+screen.blit(button_circle, (button_rect.x, button_rect.y))
+screen.blit(button_rectangle, (button_rect2.x, button_rect2.y))
+screen.blit(button_polygon, (button_rect3.x, button_rect3.y))
 pygame.display.update()
 while(running):
     for event in pygame.event.get():
@@ -28,8 +68,6 @@ while(running):
         if (event.type==pygame.MOUSEBUTTONUP):
             print("Прекращаем рисовать")
             first_pos=event.pos
-
-            startDraw=False
 pygame.quit()
 
 
